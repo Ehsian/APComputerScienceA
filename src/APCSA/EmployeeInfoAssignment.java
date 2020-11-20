802b39ed6cf7a84f6abb8f4dec1dbc654926d1a0
Ben Jia <benjia279@gmail.com>
2020-11-20 10:47:59 -0800

    Create EmployeeInfoAssignment.java

---
+++ src/APCSA/EmployeeInfoAssignment.java
@@ -0,0 +1,39 @@
+package APCSA;
+
+public class EmployeeInfoAssignment {
+    String name;
+    int number;
+    double wageRate = 15.0;
+    double yearsWorking = 0;
+    public EmployeeInfoAssignment(){
+        System.out.println("Casual Employee, no name or number needed");
+    }
+    public EmployeeInfoAssignment(String Name,int Number){
+        name = Name;
+        number = Number;
+    }
+    public void setWageRate(int num){
+        wageRate = num;
+    }
+    public double getWageRate(){
+        return wageRate;
+    }
+    public boolean isSeniorEmployee(){
+        return yearsWorking>10;
+    }
+    public double giveRaise(double raise){
+        wageRate+=raise;
+        return wageRate;
+    }
+    public double giveRaise(double raise,double yearsWorking){
+        if(yearsWorking>25){raise*=2;}
+        wageRate +=raise;
+        return wageRate;
+    }
+    public String toString(){
+        if(name == null){
+            return "No name.";
+        }
+        return name;
+    }
+}
