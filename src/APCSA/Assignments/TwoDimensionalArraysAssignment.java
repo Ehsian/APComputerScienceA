4c34dac3c22967e676978079fd6b88b1b504ed66
Ben Jia <benjia279@gmail.com>
2021-01-26 20:16:50 -0800

    Rename TwoDimensionalArraysAssignment

--- src/APCSA/Assignments/TwoDimensionalArraysAssignment.java
+++ src/APCSA/Assignments/TwoDimensionalArraysAssignment.java
@@ -1,6 +1,6 @@
 package APCSA.Assignments;
 import java.util.Arrays;
-public class TwoDimensionalArrays {
+public class TwoDimensionalArraysAssignment {
     public static void main(String[] args) {
         //1)
         int[][]magicsquare = {{16,3,2,13},{5,10,11,8},{9,6,7,12},{4,15,14,1}};
