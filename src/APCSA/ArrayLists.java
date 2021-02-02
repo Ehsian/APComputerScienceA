45433fa2b3fe19f53bfcd07edd823d9f7209da41
Ben Jia <benjia279@gmail.com>
2021-02-01 20:50:29 -0800

    Create ArrayLists.java

---
+++ src/APCSA/ArrayLists.java
@@ -0,0 +1,49 @@
+package APCSA;
+import java.util.ArrayList; //Required
+public class ArrayLists {
+    public static void main(String[] args) {
+        //n arraylist can only contain objects, not primitive data
+        //An arraylist is dynamic
+        //You do not need to declare the size beforehand
+        //It grows or shrinks as needed
+
+        //To Declare an arraylist, you must specify the object
+        ArrayList<String> list = new ArrayList<>();
+
+        //To add items, use add
+        list.add("Hi");
+        list.add("Ice cream");
+        list.add("snake");
+
+        //To traverse the list, use get and size
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+
+        //An enhanced for loop or for each loop allows you to traverse the array
+        for(String str : list){
+            System.out.println(str);
+        }
+
+        //Methods for removing all instances of item from list
+        //This type of question is common on the AP exam
+
+        //Method 1: Traverse arraylist from end
+        for(int i=list.size()-1;i>=0;i--){
+            if(list.get(i).equals("snake")){
+                list.remove(i);
+            }
+        }
+
+        //Method 2: Only move forwards if the search does not match
+        int j = 0;
+        while(j<list.size()){
+            if(list.get(j).equals("snake")){
+                list.remove(j);
+            }
+            else{
+                j++;
+            }
+        }
+    }
+}
